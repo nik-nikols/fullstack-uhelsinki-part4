17098956c7db79d3a7c871c76739d4c174156b7f
Nikolay Yarunin <nik.yarunin.dev@gmail.com>
2024-02-12 03:03:51 -0500

    Exercise 4.4

--- blogs-backend/tests/list_helper.test.js
+++ blogs-backend/tests/list_helper.test.js
@@ -6,3 +6,22 @@ test('dummy returns one', () => {
     const result = listHelper.dummy(blogs);
     expect(result).toBe(1);
 });
+
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        expect(listHelper.totalLikes([])).toBe(0);
+    });
+
+    test('when list has only one blogequals the likes of that', () => {
+        const blog = { likes: 10 };
+        expect(listHelper.totalLikes([blog])).toBe(blog.likes);
+    });
+
+    test('of a bigger list is calculated right', () => {
+        const blogs = [
+            { likes: 10 },
+            { likes: 7 },
+        ];
+        expect(listHelper.totalLikes(blogs)).toBe(17);
+    });
+});
